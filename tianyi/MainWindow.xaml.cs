57aed5747faf0e298f161527816b9af35d2b697b
凯 李 <jkuilojk123@outlook.com>
2024-09-08 16:54:46 +0800

    添加项目文件。

---
+++ tianyi/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tianyi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = new Button();
+            button.Content = "可爱";
+            Random random = new Random();
+
+            button.Height = 47;
+            button.Width = 97;
+            button.FontSize = 30;
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+            button.VerticalAlignment = VerticalAlignment.Top;
+            TranslateTransform translateTransform = new TranslateTransform();
+            translateTransform.X = random.Next(40)+160;
+            translateTransform.Y = random.Next(40)+260;
+            button.RenderTransform = translateTransform;
+            button.Click += Button_Click_1;
+            ((Grid)this.FindName("Grid")).Children.Add(button);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            PopupWindow popup = new PopupWindow();
+            popup.Owner = this;
+            //popup.PopupTextBlock.Text = "天依真的可爱";
+            popup.ShowDialog();
+            this.IsEnabled = true;
+        }
+    }
+}
---
+++ tianyi/PopupWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tianyi
+{
+    /// <summary>
+    /// PopupWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PopupWindow : Window
+    {
+        public PopupWindow()
+        {
+            InitializeComponent();
+            double screenWidth = System.Windows.SystemParameters.WorkArea.Width;
+            double screenHeight = System.Windows.SystemParameters.WorkArea.Height;
+
+            // 假设窗口宽度为 400，高度为 300
+            double windowWidth = 200;
+            double windowHeight = 100;
+
+            // 计算窗口位置
+            Left = (screenWidth - windowWidth) / 2;
+            Top = (screenHeight - windowHeight) / 2;
+            var uri = new Uri("C:\\Users\\jkuil\\source\\repos\\tianyi\\tianyi\\img\\tianyi .jpg");
+            var bitmapImage = new BitmapImage(uri);
+            Background = new ImageBrush(bitmapImage);
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Agree agree = new Agree();
+            agree.Show();
+        }
+    }
+}
